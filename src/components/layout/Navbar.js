977b0d786d0298da198be5f9f7122ace7e7e139b
splanis <splanis5@gmail.com>
2020-01-26 23:44:06 +0200

    searchbar and user added

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./Navbar.scss";
+import logo from '../../assets/icons/user-icon.png';
 
 const Navbar = () => {
-    return (
-        <div className="navbar">
-            <h1>Wolt</h1>
-        </div>
-    );
+	return (
+		<div className="navbar">
+			<h1>Wolt</h1>
+			<input type="text" name="search" id="search" placeholder="Search store, food, location.." />
+			<div className="user">
+				<img className="user-icon" src={logo} />
+				<p>Username</p>
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
