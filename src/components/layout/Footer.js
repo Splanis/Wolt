ce1e71bf59ad486a4b6fac508f9104d9bf2c59c4
splanis <splanis5@gmail.com>
2020-01-27 01:29:37 +0200

    Footer Added

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../assets/styles/Footer.scss";
+
+const Footer = () => {
+	return (
+		<div className="footer">
+			<p>Copyright</p>
+		</div>
+	);
+};
+
+export default Footer;
