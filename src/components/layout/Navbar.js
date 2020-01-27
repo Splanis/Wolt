83eae7ca390a460bd95a2be77c9c5b2b618d801e
Marios G <splanis5@gmail.com>
2020-01-27 16:22:35 +0200

    Added Wolt Logo to Navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,29 +3,26 @@ import "../../assets/styles/Navbar.scss";
 import userIcon from "../../assets/icons/user-icon.png";
 import searchIcon from "../../assets/icons/search-icon.png";
 import menuIcon from "../../assets/icons/menu-icon.png";
+import woltLogo from "../../assets/icons/wolt-logo.png";
 
 const Navbar = () => {
-	return (
-		<div className="navbar">
-			<h1>Wolt</h1>
-			<button className="mobile-menu-btn">
-				<img src={menuIcon} />
-			</button>
-			<div className="search-bar">
-				<input
-					type="text"
-					name="search"
-					id="search"
-					placeholder="Search..."
-				/>
-				<img src={searchIcon} className="search-icon" />
-			</div>
-			<div className="user">
-				<img className="user-icon" src={userIcon} />
-				<p>Username</p>
-			</div>
-		</div>
-	);
+    return (
+        <div className="navbar">
+
+            <img className="logo" src={woltLogo} alt="logo" />
+            <button className="mobile-menu-btn">
+                <img src={menuIcon} alt="menu" />
+            </button>
+            <div className="search-bar">
+                <input type="text" name="search" id="search" placeholder="Search..." />
+                <img src={searchIcon} alt="search" className="search-icon" />
+            </div>
+            <div className="user">
+                <img className="user-icon" alt="user" src={userIcon} />
+                <p>Username</p>
+            </div>
+        </div>
+    );
 };
 
 export default Navbar;
