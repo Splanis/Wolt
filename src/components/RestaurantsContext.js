248f3102ffe218c01c269d0f5fdf30cbc84ccef5
Marios G <splanis5@gmail.com>
2020-01-27 17:08:13 +0200

    Sorting fixed for Chrome

--- src/components/RestaurantsContext.js
+++ src/components/RestaurantsContext.js
@@ -4,16 +4,7 @@ import data from "../data/restaurants.json";
 export const RestaurantsContext = createContext();
 
 export const RestaurantsProvider = props => {
-	const [restaurants, sortRestaurants] = useState(
-		data["restaurants"].sort(
-			(restaurants1, restaurants2) =>
-				restaurants1.name > restaurants2.name
-		)
-	);
+    const [restaurants, sortRestaurants] = useState(data["restaurants"]);
 
-	return (
-		<RestaurantsContext.Provider value={[restaurants, sortRestaurants]}>
-			{props.children}
-		</RestaurantsContext.Provider>
-	);
+    return <RestaurantsContext.Provider value={[restaurants, sortRestaurants]}>{props.children}</RestaurantsContext.Provider>;
 };
--- src/components/Sort.js
+++ src/components/Sort.js
@@ -1,33 +1,32 @@
-import React, { useContext, Component } from "react";
+import React, { useContext } from "react";
 import { RestaurantsContext } from "./RestaurantsContext";
 
 const Sort = () => {
-	const [restaurants, sortRestaurants] = useContext(RestaurantsContext);
+    const [restaurants, sortRestaurants] = useContext(RestaurantsContext);
 
-	const sortAscending = () => {
-		sortRestaurants(restaurants =>
-			[...restaurants].sort((restaurants1, restaurants2) => restaurants1.name > restaurants2.name)
-		);
-	};
+    const handleSort = e => {
+        if (e.target.value === "ascending") {
+            sortRestaurants(restaurants =>
+                [...restaurants].sort((restaurants1, restaurants2) => (restaurants1.name < restaurants2.name ? -1 : 1))
+            );
+        } else if (e.target.value === "descending") {
+            sortRestaurants(restaurants =>
+                [...restaurants].sort((restaurants1, restaurants2) => (restaurants1.name > restaurants2.name ? -1 : 1))
+            );
+        }
+    };
 
-	const sortDescending = () => {
-		sortRestaurants(restaurants =>
-			[...restaurants].sort((restaurants1, restaurants2) => restaurants1.name < restaurants2.name)
-		);
-	};
-
-	return (
-		<div>
-			<select name="sort" id="sort">
-				<option value="ascending" onClick={sortAscending}>
-					Ascending
-				</option>
-				<option value="descending" onClick={sortDescending}>
-					Descending
-				</option>
-			</select>
-		</div>
-	);
+    return (
+        <div>
+            <select name="sort" id="sort" onChange={handleSort}>
+                <option value="nonSorted" disabled="true" selected="true">
+                    ---
+                </option>
+                <option value="ascending">Ascending</option>
+                <option value="descending">Descending</option>
+            </select>
+        </div>
+    );
 };
 
 export default Sort;
