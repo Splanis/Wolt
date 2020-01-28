af26cb863b8f1b275542cbf3d2224b9933e1b3f2
splanis <splanis5@gmail.com>
2020-01-28 12:55:09 +0200

    Sort Button Changed

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -1,32 +1,44 @@
 import React, { useContext } from "react";
 import { RestaurantsContext } from "./RestaurantsContext";
+import "../assets/styles/Sort.scss";
+import arrowDown from "../assets/icons/arrow-down.png";
 
 const Sort = () => {
-    const [restaurants, sortRestaurants] = useContext(RestaurantsContext);
+	const [restaurants, sortRestaurants] = useContext(RestaurantsContext);
 
-    const handleSort = e => {
-        if (e.target.value === "ascending") {
-            sortRestaurants(restaurants =>
-                [...restaurants].sort((restaurants1, restaurants2) => (restaurants1.name < restaurants2.name ? -1 : 1))
-            );
-        } else if (e.target.value === "descending") {
-            sortRestaurants(restaurants =>
-                [...restaurants].sort((restaurants1, restaurants2) => (restaurants1.name > restaurants2.name ? -1 : 1))
-            );
-        }
-    };
+	const handleSort = e => {
+		if (e.target.value === "ascending") {
+			sortRestaurants(restaurants =>
+				[...restaurants].sort((restaurants1, restaurants2) =>
+					restaurants1.name < restaurants2.name ? -1 : 1
+				)
+			);
+		} else if (e.target.value === "descending") {
+			sortRestaurants(restaurants =>
+				[...restaurants].sort((restaurants1, restaurants2) =>
+					restaurants1.name > restaurants2.name ? -1 : 1
+				)
+			);
+		}
+	};
 
-    return (
-        <div>
-            <select name="sort" id="sort" onChange={handleSort}>
-                <option value="nonSorted" disabled="true" selected="true">
-                    ---
-                </option>
-                <option value="ascending">Ascending</option>
-                <option value="descending">Descending</option>
-            </select>
-        </div>
-    );
+	return (
+		<div className="select-box">
+			<select
+				
+				name="sort"
+				id="sort"
+				onChange={handleSort}
+			>
+				<option value="nonSorted" disabled="true" selected="true">
+					Sort By
+				</option>
+				<option value="ascending">Ascending</option>
+				<option value="descending">Descending</option>
+			</select>
+
+		</div>
+	);
 };
 
 export default Sort;
