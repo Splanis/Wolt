99c3ff99462e401cab9feee28abef7246b531501
Marios G <splanis5@gmail.com>
2020-01-28 22:55:00 +0200

    Sidenav Deleted

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
 import "./assets/styles/App.scss";
 import { RestaurantsProvider } from "./components/RestaurantsContext";
-import RestaurantList from "./components/RestaurantList";
+import Restaurants from "./components/Restaurants";
 import Navbar from "./components/layout/Navbar";
-import Sidenav from "./components/layout/Sidenav";
 import Footer from "./components/layout/Footer";
 
 const App = () => {
@@ -11,9 +10,8 @@ const App = () => {
         <RestaurantsProvider>
             <div className="App">
                 <Navbar />
-                <Sidenav className="sidenav" />
                 <main>
-                    <RestaurantList />
+                    <Restaurants />
                 </main>
                 <Footer />
             </div>
