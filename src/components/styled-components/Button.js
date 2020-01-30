15e37e23776e58eb7a7a3c81d6756cad975805c0
Marios G <splanis5@gmail.com>
2020-01-31 00:16:24 +0200

    Default Button Added

---
+++ src/components/styled-components/Button.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+    padding: 5px 3px;
+    margin: 5px;
+    background-color: transparent;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: none;
+    border-radius: 4px;
+    font-size: 1rem;
+    box-shadow: -2px -2px 8px rgba(255, 255, 255, 1), -2px -2px 12px rgba(255, 255, 255, 0.5), inset 2px 2px 4px rgba(255, 255, 255, 0.1),
+        2px 2px 8px rgba(0, 0, 0, 0.15);
+
+    &:hover {
+        box-shadow: inset -2px -2px 8px rgba(255, 255, 255, 1), inset -2px -2px 12px rgba(255, 255, 255, 0.5),
+            inset 2px 2px 4px rgba(255, 255, 255, 0.1), inset 2px 2px 8px rgba(0, 0, 0, 0.15);
+    }
+`;
+
+export default Button;
