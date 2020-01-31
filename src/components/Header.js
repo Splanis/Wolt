9b301b8e774094f557c57d05b79d724f14015832
Marios G <splanis5@gmail.com>
2020-01-31 16:24:57 +0200

    Moved layout Buttons to new Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,16 @@
 import React from "react";
 import styled from "styled-components";
-import gridIcon from "../assets/icons/grid.png";
-import listIcon from "../assets/icons/list.png";
-import Button from "./styled-components/Button";
 import Sort from "./Sort";
+import LayoutToggle from "./LayoutToggle";
 
 const Header = ({ restaurantsOnline, toGrid, toList }) => {
     return (
         <HeaderContain>
-            <h2 className="open-restaurants">{restaurantsOnline} Restaurants online</h2>
-            <div className="buttons">
+            <OpenRestaurants>{restaurantsOnline} Restaurants online</OpenRestaurants>
+            <Buttons>
                 <Sort />
-                <div className="layout-btns">
-                    <Button className="grid-btn" onClick={toGrid}>
-                        <img src={gridIcon} alt="grid" />
-                    </Button>
-                    <Button className="list-btn" onClick={toList}>
-                        <img className="list-img" src={listIcon} alt="list" />
-                    </Button>
-                </div>
-            </div>
+                <LayoutToggle toGrid={toGrid} toList={toList} />
+            </Buttons>
         </HeaderContain>
     );
 };
@@ -27,48 +18,21 @@ const Header = ({ restaurantsOnline, toGrid, toList }) => {
 const HeaderContain = styled.div`
     margin: 10px;
 
-    .buttons {
-        margin-top: 15px;
-        display: flex;
-        justify-content: space-between;
-    }
-
-    .layout-btns {
-        margin-left: 20px;
-        display: flex;
-
-        img {
-            height: 20px;
-        }
-
-        .list-img {
-            transform: scale(1.15);
-        }
-
-        &:focus {
-            outline: none;
-        }
-    }
-
-    @media (max-width: 600px) {
+    @media (max-width: 750px) {
         display: flex;
         flex-direction: column;
         align-items: center;
-
-        .open-restaurants {
-            font-size: 1.3rem;
-            text-align: center;
-        }
-
-        .sort {
-            margin: auto;
-            margin-top: 10px !important;
-        }
-
-        .layout-btns {
-            display: none !important;
-        }
     }
+`;
+
+const OpenRestaurants = styled.h1`
+    font-size: 1.5rem;
+`;
+
+const Buttons = styled.div`
+    margin-top: 15px;
+    display: flex;
+    justify-content: space-between;
 `;
 
 export default Header;
---
+++ src/components/LayoutToggle.js
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components";
+import gridIcon from "../assets/icons/grid.png";
+import listIcon from "../assets/icons/list.png";
+import Button from "./styled-components/Button";
+
+const LayoutToggle = ({ toGrid, toList }) => {
+    return (
+        <LayoutButtons>
+            <Button onClick={toGrid}>
+                <img src={gridIcon} alt="grid" />
+            </Button>
+            <Button onClick={toList}>
+                <img style={{transform: "scale(1.15)"}} src={listIcon} alt="list" />
+            </Button>
+        </LayoutButtons>
+    );
+};
+
+const LayoutButtons = styled.div`
+    display: flex;
+
+    img {
+        height: 20px;
+    }
+
+    &:focus {
+        outline: none;
+    }
+
+    @media (max-width: 750px) {
+        display: none;
+    }
+`;
+
+export default LayoutToggle;
