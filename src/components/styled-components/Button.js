690659e16124617827b6d32279e6a09a776b023e
Marios G <splanis5@gmail.com>
2020-01-31 14:18:49 +0200

    outline none to buttons

--- src/components/styled-components/Button.js
+++ src/components/styled-components/Button.js
@@ -17,6 +17,10 @@ const Button = styled.button`
         box-shadow: inset -2px -2px 8px rgba(255, 255, 255, 1), inset -2px -2px 12px rgba(255, 255, 255, 0.5),
             inset 2px 2px 4px rgba(255, 255, 255, 0.1), inset 2px 2px 8px rgba(0, 0, 0, 0.15);
     }
+
+    &:focus {
+        outline: none;
+    }
 `;
 
 export default Button;
