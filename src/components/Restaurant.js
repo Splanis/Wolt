605eb06eae033daef3f2f9c3ffd9c950f2e47ac9
Marios G <splanis5@gmail.com>
2020-01-31 15:20:16 +0200

    transition removed

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -52,7 +52,6 @@ const RestaurantCard = styled.div`
     height: ${props => (props.isGrid ? "400px" : "170px")};
     border-radius: 3px;
     box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-    transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
     margin: 10px;
 
     &:hover {
