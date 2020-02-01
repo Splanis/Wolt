864ab81afb7b4ef8804c4797e6dedf309dc0840e
Marios G <splanis5@gmail.com>
2020-02-01 17:26:28 +0200

    Added ActiveButton

--- src/components/SharedStyles/Button.js
+++ src/components/SharedStyles/Button.js
@@ -24,3 +24,6 @@ const Button = styled.button`
 `;
 
 export default Button;
+
+export const ActiveButton = `inset -2px -2px 8px rgba(255, 255, 255, 1), inset -2px -2px 12px rgba(255, 255, 255, 0.5),
+        inset 2px 2px 4px rgba(255, 255, 255, 0.1), inset 2px 2px 8px rgba(0, 0, 0, 0.15)`;
