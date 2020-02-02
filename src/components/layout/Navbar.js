58cee74b3649e2d7f93437030eaa0deb1fb7e073
Marios G <splanis5@gmail.com>
2020-02-02 03:42:33 +0200

    New svg logo added

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,39 +1,26 @@
 import React from "react";
-import woltLogo from "../../assets/icons/wolt-logo.png";
-import styled, { keyframes } from "styled-components";
+import WoltLogo from "../SVGs/WoltLogo";
+import styled from "styled-components";
 
 const Navbar = () => {
     return (
         <NavBarContain>
-            <Logo src={woltLogo}></Logo>
+            <WoltLogo></WoltLogo>
         </NavBarContain>
     );
 };
 
 // Components' style
-const NavBarAnimation = keyframes`
-    from {
-        margin-bottom: 120px;
-    }
-`;
-
 const NavBarContain = styled.nav`
     background-color: #333333;
     height: 70px;
     display: flex;
-    justify-content: flex-start;
+    justify-content: center;
     align-items: center;
     position: fixed;
     width: 100%;
     box-shadow: 0px 2px 14px 2px rgba(0, 0, 0, 0.6);
     z-index: 10;
-`;
-
-const Logo = styled.img`
-    height: 60px;
-    margin-left: 10px;
-    width: 150px;
-    animation: ${NavBarAnimation} 0.8s;
 `;
 
 export default Navbar;
