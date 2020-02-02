8bbad0492b89ef3c80083b9821a3cf3f99b31147
Marios G <splanis5@gmail.com>
2020-02-02 14:45:21 +0200

    Logo moved to left

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,11 @@
 import React from "react";
-import WoltLogo from "../SVGs/WoltLogo";
+import MainLogo from "../SVGs/MainLogo";
 import styled from "styled-components";
 
 const Navbar = () => {
     return (
         <NavBarContain>
-            <WoltLogo />
+            <MainLogo />
         </NavBarContain>
     );
 };
@@ -15,11 +15,15 @@ const NavBarContain = styled.nav`
     background-color: #333333;
     height: 70px;
     display: flex;
-    justify-content: center;
-    align-items: center; 
+    justify-content: flex-start;
+    align-items: center;
     width: 100%;
     box-shadow: 0px 2px 14px 2px rgba(0, 0, 0, 0.6);
     z-index: 10;
+
+    & :nth-child(1) {
+        margin-left: 10px;
+    }
 `;
 
 export default Navbar;
