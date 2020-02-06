083abd93434201158225db303f6c7466341a06a2
Marios G <splanis5@gmail.com>
2020-02-06 19:56:02 +0200

    changed dispatch to action

--- src/components/context/RestaurantsContext.js
+++ src/components/context/RestaurantsContext.js
@@ -4,8 +4,8 @@ import data from "../../data/restaurants.json";
 // Getting restaurants data from the .json file
 const restaurantsData = data["restaurants"];
 
-const sortReducer = (restaurants, dispatch) => {
-    switch (dispatch.type) {
+const sortReducer = (restaurants, action) => {
+    switch (action.type) {
         case "ascending":
             return [...restaurants].sort((restaurants1, restaurants2) => (restaurants1.name < restaurants2.name ? -1 : 1));         
         case "descending": 
@@ -14,14 +14,15 @@ const sortReducer = (restaurants, dispatch) => {
             return [...restaurantsData];    
     }
 }
+// Used '? -1 : 1' to handle sorting at Google Chrome and Safari
 
 export const RestaurantsContext = createContext();
 
 export const RestaurantsProvider = props => {
-    const [restaurants, sortRestaurants] = useReducer(sortReducer, restaurantsData);
+    const [restaurants, dispatch] = useReducer(sortReducer, restaurantsData);
     
     return (
-        <RestaurantsContext.Provider value={[restaurants, sortRestaurants]}>
+        <RestaurantsContext.Provider value={[restaurants, dispatch]}>
             {props.children}
         </RestaurantsContext.Provider>
     );
